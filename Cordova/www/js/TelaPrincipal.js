7f90a0bdd9548fae01b0c8890cc9ac6f6764f3c8
Matheus Vinicius Kuc <matheus.v.kuc@gmail.com>
2023-04-07 19:26:42 -0300

    fix: Ajustes

--- Cordova/www/js/TelaPrincipal.js
+++ Cordova/www/js/TelaPrincipal.js
@@ -1,28 +1,36 @@
 let id = 0
 let valorTotal = 0
 let placaConsultar = ''
-const usuario = 'Matheus_Kuc' // Simula um login
+const usuario = 'Matheus Kuc' // Simula um login
 
 function enviar() {
     const placa_informada = document.getElementById("num_placa").value
     if (placa_informada.length < 7) {
         dialog("Placa Inválida")
+        document.getElementById("num_placa").value = "";
         return
     }
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var cliente = JSON.parse(this.responseText);
-            console.log(cliente)
+            const cliente = JSON.parse(this.responseText)
+            const dataEntrada = cliente.entrada
+            const saida = cliente.saida
+
+            const data = new Date();
+            const dataFormatada = data.toISOString().substr(0, 19).replace('T', ' ');
+
+            if (dataEntrada === dataFormatada && (saida === null || saida !== false)) {
+                dialog("Veículo registrado com sucesso!")
+            } else {
+                dialog("Veículo já registrado!")
+            }
         }
     }
 
-    xmlhttp.open("GET", "https://hernanicruz.com/aulas/estacionamento/registrarEntradaGet.php?placa=" + placa_informada + "&user=TESTE", false);
+    xmlhttp.open("GET", "https://hernanicruz.com/aulas/estacionamento/registrarEntradaGet.php?placa=" + placa_informada + "&user=" + usuario, true);
     xmlhttp.send();
-
-    // const resp = registrarEntrada(placa_informada, usuario)
-    // dialog(resp)
 
     document.getElementById("num_placa").value = "";
 }
@@ -34,39 +42,53 @@ function consultar() {
         return;
     }
 
-    const resp = prePagamento(placa_informada)
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText === 'Veículo não encontrado') {
+                dialog(this.responseText)
+                document.getElementById("num_placa").value = "";
+                return;
+            }
 
-    if (resp.error !== undefined) {
-        dialog(resp.error)
-        return
+            const cliente = JSON.parse(this.responseText)
+            const entrada = cliente.entrada
+            const placa = cliente.placa
+            const valorPorHora = 5
+            id = cliente.id
+
+            dialog(mensagemConsulta(entrada, valorPorHora, placa))
+        }
     }
 
-    id = resp.id;
-
-    var horas = resp.horas
-    var placa = resp.placa;
-    var valorPorHora = 15;
-
-    valorTotal = valorPorHora * horas;
-    placaConsultar = placa_informada;
-
-    var msg = "O veículo com placa " + placa + " ficou por " + horas + " horas. Valor por hora de R$" + valorPorHora + ",00. Total do estacionamento ficou em R$" + valorTotal + ",00.";
-
-    dialog(msg)
+    xmlhttp.open("GET", "https://hernanicruz.com/aulas/estacionamento/prePagamentoGet.php?placa=" + placa_informada, true);
+    xmlhttp.send();
     document.getElementById("buttonPagamento").disabled = false;
 }
 
 function pagamento() {
     var placa_informada = document.getElementById("num_placa").value;
     if (placa_informada.length < 7) {
+        document.getElementById("num_placa").value = "";
         dialog("Placa Inválida");
         return;
     }
 
     if (placa_informada === placaConsultar) {
-        const resp = registrarPagamento(placa_informada, valorTotal)
-        
-        dialog(resp)
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                const cliente = JSON.parse(this.responseText)
+                if (cliente.mensagem !== undefined) {
+                    dialog("Pagamento efetuado com sucesso!")
+                } else {
+                    dialog("Já foi realizado o pagamento!")
+                }
+            }
+        }
+
+        xmlhttp.open("GET", "https://hernanicruz.com/aulas/estacionamento/registrarPagamentoGet.php?id=" + id + "&valor=" + valorTotal, true);
+        xmlhttp.send();
     } else {
         dialog("A placa " + placa_informada + " informada é diferente da placa " + placaConsultar + " consultada! Consulte essa placa antes de fazer o pagamento.");
     }
@@ -87,62 +109,38 @@ function dialog(mensagem) {
     document.getElementById("dialog").open = true;
 }
 
-const veiculos = []
+function mensagemConsulta(entrada, valorHora, placa) {
+    const dataEntrada = new Date(entrada)
+    const dataAtual = new Date()
 
-function criarVeiculo(placa, usuario) {
-    const cadastro = {
-        "id": Math.random(),
-        "horario_server": new Date(),
-        "entrada": new Date(),
-        "placa": placa,
-        "saida": false,
-        "valor": null,
-        "user": usuario,
-        "horas": 3
-    }
-    veiculos.push(cadastro)
+    const diferencaMili = (dataAtual - dataEntrada)
+    const minutos = verifarMinutos(diferencaMili)
+    const horas = Math.floor((diferencaMili / 1000) / 3600)
+    valorTotal = valorHora * horas
+
+    const dataEntradaFormatada = dataEmFormatoBr(dataEntrada)
+    const dataAtualFormatada = dataEmFormatoBr(dataAtual)
+
+    return "O veículo com placa " + placa + " ficou por " + horas + " horas e " + minutos + " minutos. Valor por hora de R$" + valorHora + ",00. Total do estacionamento ficou em R$" + valorTotal + ",00. Ficou da data " + dataEntradaFormatada + " até " + dataAtualFormatada + "."
 }
 
-function registrarEntrada(placa, usuario) {
-    let notExist = true
-    veiculos.forEach(v => {
-        if (placa === v.placa && v.saida === false) {
-            notExist = false
-        }
-    })
-
-    if (notExist) {
-        criarVeiculo(placa, usuario)
-        return "Registrado com sucesso!"
+function verifarMinutos(milisegundos) {
+    const minutos = Math.floor((milisegundos / 1000) / 60)
+    const qtdHoras = Math.floor(minutos / 60);
+    if (qtdHoras > 0) {
+        return minutos - 60 * qtdHoras
     }
 
-    return "Veículo já esta cadastrado!"
+    return minutos
 }
 
-function prePagamento(placa) {
-    let exist = false
-    let veiculo
-    veiculos.forEach(v => {
-        if (placa === v.placa && v.saida === false) {
-            exist = true
-            veiculo = v
-        }
-    })
+function dataEmFormatoBr(data) {
+    const dia = data.getDate().toString().padStart(2, '0');
+    const mes = (data.getMonth() + 1).toString().padStart(2, '0');
+    const ano = data.getFullYear().toString();
+    const hora = data.getHours().toString().padStart(2, '0');
+    const minutos = data.getMinutes().toString().padStart(2, '0');
+    const segundos = data.getSeconds().toString().padStart(2, '0');
 
-    if (!exist) return { error: 'Não existe nenhum veículo com essa placa' };
-
-    return veiculo
-}
-
-function registrarPagamento(placa, valor) {
-    let veiculo
-    veiculos.forEach(v => {
-        if (placa === v.placa && v.saida === false) {
-            veiculo = v
-        }
-    }) 
-    veiculo.saida = true
-    veiculo.valor = valor
-
-    return 'Pagamento efetuado com sucesso! No valor de R$' + valor + ',00.'
+    return `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
 }
